e8a3e8345f6d2132a3b2a66eaa63b857699a60f5
Stephen Vizzusi <svizzusi13@gmail.com>
2023-01-18 16:45:37 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Stephen is the champion";
+document.body.append(newHeader);
